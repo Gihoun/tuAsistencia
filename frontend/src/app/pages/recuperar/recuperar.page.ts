b8f7e546c2736f20691a2c47d2ec2dd5030589d2
Mauro <ma.candias@duocuc.cl>
2021-11-08 04:14:14 -0300

    ok

--- frontend/src/app/pages/recuperar/recuperar.page.ts
+++ frontend/src/app/pages/recuperar/recuperar.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+import { NavController, ToastController } from '@ionic/angular';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-recuperar',
@@ -6,11 +10,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recuperar.page.scss'],
 })
 export class RecuperarPage implements OnInit {
+  // Titulo pagina.
+  Title = 'Recuperar Contraseña';
+  // contenedor para realizar update.
+  usaurio: any = [];
 
-  Title ="Recuperar Contraseña"
-  constructor() { }
+  constructor(
+    private userServices: UsersService,
+    private router: Router,
+    private activatedRouter: ActivatedRoute,
+    private toastController: ToastController,
+    private navCtrl: NavController
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  async crearToast(mensaje: string, duracion: number) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: duracion,
+    });
+    toast.present();
   }
 
+  CambiarPass(email) {
+    this.userServices.getUserMail(email.value).subscribe((res) => {
+      this.usaurio = res;
+      if (this.usaurio[0] == undefined) {
+        this.crearToast('Correo No Existe', 3000);
+      } else {
+        this.userServices
+          .updateUserPass(this.usaurio[0].id, {
+            fName: this.usaurio[0].fName,
+            lName: this.usaurio[0].lName,
+            uMail: this.usaurio[0].uMail,
+            uPass: formatDate(
+              this.usaurio[0].uBirth,
+              'dd/MM/yyyy',
+              'en-US',
+              '-0400'
+            ),
+          })
+          .subscribe(
+            (update) => {
+              this.crearToast(
+                'Contraseña restablecida, ocupe su fecha de nacimiento como contraseña (dd/MM/yyy)',
+                3000
+              );
+            },
+            (err) => this.crearToast('Correo No Existe', 3000)
+          );
+      }
+    });
+  }
 }
--- frontend/src/app/services/users.service.ts
+++ frontend/src/app/services/users.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+export interface Usuario{
+  id?: string;
+  fName: string;
+  lName: string;
+  uMail: string;
+  uPass: string;
+  uBirth?: Date;
+  user_rol?: any;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +22,7 @@ export class UsersService {
   API = 'http://localhost:1337/app-users';
   // Ruta libro asistencia.
   APIAsistencia = 'http://localhost:1337/asistencias';
- 
+
  
   //METODOS CRUD
 
@@ -29,7 +38,7 @@ export class UsersService {
   getUser(email: String, pass: String) {
     return this.client.get(this.API + '?uMail=' + email + '&uPass=' + pass);
   }
-  //Obtienes los datos de un usuario unico
+  //Obtienes los datos de un usuario unico por id
   getUserprofile(id: String) {
     return this.client.get(this.API + '/' + id);
   }
@@ -55,7 +64,10 @@ export class UsersService {
   deleteAlumno(id: string) {
     return this.client.delete(`${this.API}/${id}`);
   }
-
-
-
+  //
+  getUserMail(uMail: string){
+    return this.client.get(this.API + '?uMail=' + uMail)
+  }
+  updateUserPass(id: string, usuario: Usuario){
+    return this.client.put(`${this.API}/${id}`, usuario)}
 }
